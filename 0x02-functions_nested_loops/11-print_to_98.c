e752f88587afc8c842c1d7c5085ae4b681794015
Mbali <mbali940401@gmail.com>
2023-02-21 18:00:48 -0800

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,17 +12,17 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-	for (; n <= 98: n++)
+	for (; n <= 98; n++)
 	{
 	if (n == 98)
 	{
-	printif("%d", n);
-	printif("\n");
+	printf("%d", n);
+	printf("\n");
 	break;
 	}
 	else
 	{
-	printif("%d, ", n);
+	printf("%d, ", n);
 	}
 	}
 	}
@@ -32,13 +32,13 @@ void print_to_98(int n)
 	{
 	if (n == 98)
 	{
-	printif("%d", n);
-	printif("\n");
+	printf("%d", n);
+	printf("\n");
 	break;
 	}
 	else
 	{
-	printif("%d, ", n);
+	printf("%d, ", n);
 	}
 	}
 	}
